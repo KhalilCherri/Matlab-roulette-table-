4b5194c45956238f818204a605642a7328850c12
KhalilCherri <80485828+KhalilCherri@users.noreply.github.com>
2021-03-11 18:27:21 -0500

    Add files via upload

---
+++ oct-3_1.m
@@ -0,0 +1,28 @@
+﻿clc
+clear
+
+display('Welcome to Engr-125 Casino')
+
+money=input('How much you want to bet: ')
+type=input('Tell me what type of bet, for single enter 1, for dozen enter 2, for odd/even enter 3, for high/low, enter 4:')
+
+if type==1
+single=input('please enter your bet for a single number between 1 and 36')
+end
+
+roll=randi([1 38])
+
+fprintf('The roll is at %d', roll)
+
+% calculate the payout 
+% payout for type one 
+
+if type==1&&(single==roll)
+    payout=35*money
+    money=money+payout
+else
+    money=0
+    disp('You Lost')
+end
+
+money=money+payout
